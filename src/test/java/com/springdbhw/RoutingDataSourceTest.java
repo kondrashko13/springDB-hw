4f14fa768cf238784b51c642d811d5404afd28df
Tymko <kondrashko13@proton.me>
2026-02-25 13:11:40 +0200

    lab 5.1

--- src/test/java/com/springdbhw/RoutingDataSourceTest.java
+++ src/test/java/com/springdbhw/RoutingDataSourceTest.java
@@ -4,16 +4,10 @@ import com.springdbhw.datasource.ReplicaAwareRoutingDataSource;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
-import org.testcontainers.containers.ComposeContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import javax.sql.DataSource;
-import java.io.File;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -21,32 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
-@Testcontainers
 class RoutingDataSourceTest {
-
-    @Container
-    public static ComposeContainer environment = new ComposeContainer(
-            new File("src/test/resources/compose.yaml")
-    )
-            .withExposedService("postgres-primary", 5432)
-            .withExposedService("postgres-replica", 5432)
-            .waitingFor("postgres-primary", Wait.forListeningPort());
-
-    @DynamicPropertySource
-    static void registerDatabaseProperties(DynamicPropertyRegistry registry) {
-        String primaryHost = environment.getServiceHost("postgres-primary", 5432);
-        Integer primaryPort = environment.getServicePort("postgres-primary", 5432);
-
-        registry.add("spring.datasource.primary.jdbc-url",
-                () -> String.format("jdbc:postgresql://%s:%d/mydb", primaryHost, primaryPort));
-
-        String replicaHost = environment.getServiceHost("postgres-replica", 5432);
-        Integer replicaPort = environment.getServicePort("postgres-replica", 5432);
-
-        registry.add("spring.datasource.replica.jdbc-url",
-                () -> String.format("jdbc:postgresql://%s:%d/mydb", replicaHost, replicaPort));
-    }
-
     private DataSource primaryMock;
     private DataSource replicaMock;
     private ReplicaAwareRoutingDataSource routingDataSource;
